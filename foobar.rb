1c9e9d7c80d511d290364e4db975a4ecb05f3f84
warrenshen <warrenzshen@gmail.com>
2016-09-13 17:34:52 -0700

    Publish fa16-hw1

---
+++ foobar.rb
@@ -0,0 +1,19 @@
+class Foobar
+
+  def self.baz(a)
+    # Class method
+    # Call with `Foobar.baz`
+  end
+
+  def foo(b)
+    # Instance method
+    # Call with foobar_instance.foo
+  end
+
+  def bar(c)
+    # Instance method
+    # Call with foobar_instance.bar
+  end
+end
+
+
