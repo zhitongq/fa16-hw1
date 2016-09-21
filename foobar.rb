04ac6af350fe2bbcd964f66a17f3609c72f452bd
zhitongq <zhiqiu@berkeley.edu>
2016-09-21 00:52:32 -0700

    Update foobar.rb

--- foobar.rb
+++ foobar.rb
@@ -3,6 +3,11 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    new_arr = Array.new
+    new_arr = a.map { |x| x.to_i + 2 }
+    new_arr = new_arr.values_at(* a.each_index.select {|x| x if new_arr[x] < 10 && x.odd?})
+    new_arr = new_arr.uniq
+    new_arr = new_arr.inject(0){|sum,x| sum + x }
   end
 
   def foo(b)
