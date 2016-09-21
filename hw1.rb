279a45eaef419a6f5e1e4ff2ea5fb9c4d436d011
zhitongq <zhiqiu@berkeley.edu>
2016-09-21 00:51:06 -0700

    Update hw1.rb

--- hw1.rb
+++ hw1.rb
@@ -1,20 +1,16 @@
 def squared_sum(a, b)
-  # Q1 CODE HERE
-
+  (a + b) ** 2
 end
 
 def sort_array_plus_one(a)
-  # Q2 CODE HERE
-
+  a.sort!.map { |x| x + 1 }
 end
 
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
-
+  "#{first_name} #{last_name}"
 end
 
 def blockin_time(a)
-  # DO NOT EDIT THIS CODE BELOW
-  require './foobar'
-
+  require './foobar' 
+  Foobar.baz a
 end
